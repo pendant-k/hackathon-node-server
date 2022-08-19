56c7a18cd31ee76237465a91f3a43d79df0d060e
Donghan Kim <donghankim@Donghanui-MacBookPro.local>
2022-08-19 16:54:51 +0900

    Fix User model

--- models/users.js
+++ models/users.js
@@ -2,41 +2,37 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const {
-  Types: { ObjectId },
+    Types: { ObjectId },
 } = Schema;
 
 const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        nickname: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        userId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        bookmark: [
+            {
+                type: ObjectId,
+                ref: "Question",
+            },
+        ],
     },
-    nickname: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    userId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    bookmark: [
-      {
-        type: ObjectId,
-        ref: "Question",
-      },
-    ],
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
