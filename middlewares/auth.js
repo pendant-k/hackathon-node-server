af444c76430e735d185273663373c1b127ffc390
Donghan Kim <donghankim@Donghanui-MacBookPro.local>
2022-08-19 23:26:51 +0900

    Add env variable

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY =
+    "WzfdOYaZ5c6YrE+7+pS7Hn8ywz3Pqy1DFFYTMAgLmm6dTPggcfrKXpogZ4wNLZeO2fBo2BXuFJhnyV+WeUWOIVDL/5gYoxpTitaUw9H+5ou6nLr7Z8irlk0FDpsmpzfHdOzioGF8V6KUeqTx8Z6oIJcqtS/j/DbG/Yy7CyrLpHHw7yVTzOgrG6f0BKgxFbo4TyT9Up4WRV6XJpeS7w5Oa08I6PweN+NPV+ElP42XrPEUEYA5BM6712HfHMDgGCQBTubuLDP876c6+gJV8J0OgRL4/g45NJFQblMN7fr+oBEyOnCHPMzEolJ3Nfhi0AtHjX6oq6pyl2la26nwAhYRZA==";
 
 const verifyToken = (req, res, next) => {
     try {
